77d96d9f8cc3a540e1ba7ca1edfb8aa1a6893f00
Ben Cooper <78227837+bencooper01@users.noreply.github.com>
2021-03-31 12:58:33 -0700

    re remove image

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -56,7 +56,7 @@ const useStyles = makeStyles({
 		height: 125,
 		width: 120,
 		marginBottom: 5,
-		borderRadius: 2,
+		borderRadius: 3,
 		marginRight: 10,
 		alignSelf: "center",
 	},
@@ -70,23 +70,22 @@ function Home() {
 			<Container maxWidth="md">
 				<GithubCorner href="https://github.com/bencooper01" />
 
-				<Box pt={5} pb={4} display="flex" justifyContent="center" flexDirection="column">
-					<img src={benImg} className={styles.benImg} />
-					<Typography variant="h4" style={{ color: "#14487f", textAlign: "center" }}>
+				<Box pt={8} pb={4} display="flex" justifyContent="center" flexDirection="column">
+					<Typography variant="h4" style={{ color: "#14487f" }}>
 						Ben Cooper
 						<IconButton href="https://twitter.com/bend_cooper" disableRipple={true} edge={false} style={{ outline: "none", marginLeft: 3 }} size="small">
 							<Twitter className={styles.twitter} />
 						</IconButton>
 					</Typography>
 
-					<Typography variant="body2" className={styles.text} style={{ fontSize: 19, textAlign: "center" }}>
+					<Typography variant="body2" className={styles.text}>
 						I'm a 17 year old developer, currently working on Every Two (iOS and Android). Email me anytime at bdcooper14@gmail.com.
 						<br />
 						{/* Email me anytime at ben@bencooper.net! */}
 					</Typography>
 				</Box>
 
-				<Typography variant="h4" style={{ textAlign: "center" }}>
+				<Typography variant="h4">
 					Projects
 					<Divider />
 				</Typography>
